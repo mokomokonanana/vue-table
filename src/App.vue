43a9b5638d15cb9dec601a3aeb3c70a25dfd88ec
mokomokonanana <kimama.de.kimagure.chibiakuma+github@gmail.com>
2020-01-09 18:18:24 +0900

    yarn add --dev pug pug-plain-loader

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    img(alt="Vue logo" src="./assets/logo.png")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
